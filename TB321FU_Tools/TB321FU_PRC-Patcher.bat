21050021d2c4ced083ad4a6e01547a6c178c8979
MONE FIERA <83483662+monefiera@users.noreply.github.com>
2024-12-06 04:31:53 +0900

    Update TB321FU_PRC-Patcher.bat

--- TB321FU_Tools/TB321FU_PRC-Patcher.bat
+++ TB321FU_Tools/TB321FU_PRC-Patcher.bat
@@ -6,44 +6,44 @@ if /I "%c%" EQU "Y" goto :continue
 goto :end
 :continue
 
-adb shell pm uninstall --user 0 com.lenovo.legionzone || pause
-adb shell pm uninstall --user 0 com.zui.notes || pause
-adb shell pm uninstall --user 0 com.zui.calendar.overlay.paintingr || pause
-adb shell pm uninstall --user 0 com.tencent.qqmusic || pause
-adb shell pm uninstall --user 0 com.lenovo.minigamelauncher || pause
-adb shell pm uninstall --user 0 com.sohu.inputmethod.sogou.oem || pause
-adb shell pm uninstall --user 0 com.zui.calendar.overlay.golden || pause
-adb shell pm uninstall --user 0 io.moreless.tide || pause
-adb shell pm uninstall --user 0 com.zui.calendar.overlay.avengers || pause
-adb shell pm uninstall --user 0 com.smile.gifmaker || pause
-adb shell pm uninstall --user 0 com.sina.weibo || pause
-adb shell pm uninstall --user 0 com.motorola.mobiledesktop || pause
-adb shell pm uninstall --user 0 com.ss.android.ugc.aweme || pause
-adb shell pm uninstall --user 0 com.qiyi.video.pad || pause
-adb shell pm uninstall --user 0 com.zui.calendar || pause
-adb shell pm uninstall --user 0 com.zui.calculator || pause
-adb shell pm uninstall --user 0 com.youku.phone || pause
-adb shell pm uninstall --user 0 com.zui.calendar.overlay.grace || pause
-adb shell pm uninstall --user 0 com.zui.calendar.overlay.superhero || pause
-adb shell pm uninstall --user 0 com.newskyer.draw || pause
-adb shell pm uninstall --user 0 com.zui.weather || pause
-adb shell pm uninstall --user 0 com.zui.calendar.overlay.mostbeautiful || pause
-adb shell pm uninstall --user 0 com.zui.calendar.overlay.thin || pause
-adb shell pm uninstall --user 0 cn.wps.moffice_eng || pause
-adb shell pm uninstall --user 0 com.tblenovo.center || pause
-adb shell pm uninstall --user 0 com.zui.calendar.overlay.pink || pause
-adb shell pm uninstall --user 0 com.lenovo.club.app || pause
-adb shell pm uninstall --user 0 com.zui.recorder || pause
-adb shell pm uninstall --user 0 net.huanci.hsjpro || pause
-adb shell pm uninstall --user 0 com.zui.calendar.overlay.blue || pause
-adb shell pm uninstall --user 0 com.zui.calendar.overlay.mains || pause
-adb shell pm disable-user com.lenovo.leos.appstore || pause
-adb shell pm disable-user com.lenovo.menu_assistant.hd || pause
-adb shell pm disable-user com.android.providers.contacts || pause
-adb shell pm disable-user com.zui.browser || pause
+adb shell pm uninstall --user 0 com.lenovo.legionzone
+adb shell pm uninstall --user 0 com.zui.notes
+adb shell pm uninstall --user 0 com.zui.calendar.overlay.paintingr
+adb shell pm uninstall --user 0 com.tencent.qqmusic
+adb shell pm uninstall --user 0 com.lenovo.minigamelauncher
+adb shell pm uninstall --user 0 com.sohu.inputmethod.sogou.oem
+adb shell pm uninstall --user 0 com.zui.calendar.overlay.golden
+adb shell pm uninstall --user 0 io.moreless.tide
+adb shell pm uninstall --user 0 com.zui.calendar.overlay.avengers
+adb shell pm uninstall --user 0 com.smile.gifmaker
+adb shell pm uninstall --user 0 com.sina.weibo
+adb shell pm uninstall --user 0 com.motorola.mobiledesktop
+adb shell pm uninstall --user 0 com.ss.android.ugc.aweme
+adb shell pm uninstall --user 0 com.qiyi.video.pad
+adb shell pm uninstall --user 0 com.zui.calendar
+adb shell pm uninstall --user 0 com.zui.calculator
+adb shell pm uninstall --user 0 com.youku.phone
+adb shell pm uninstall --user 0 com.zui.calendar.overlay.grace
+adb shell pm uninstall --user 0 com.zui.calendar.overlay.superhero
+adb shell pm uninstall --user 0 com.newskyer.draw
+adb shell pm uninstall --user 0 com.zui.weather
+adb shell pm uninstall --user 0 com.zui.calendar.overlay.mostbeautiful
+adb shell pm uninstall --user 0 com.zui.calendar.overlay.thin
+adb shell pm uninstall --user 0 cn.wps.moffice_eng
+adb shell pm uninstall --user 0 com.tblenovo.center
+adb shell pm uninstall --user 0 com.zui.calendar.overlay.pink
+adb shell pm uninstall --user 0 com.lenovo.club.app
+adb shell pm uninstall --user 0 com.zui.recorder
+adb shell pm uninstall --user 0 net.huanci.hsjpro
+adb shell pm uninstall --user 0 com.zui.calendar.overlay.blue
+adb shell pm uninstall --user 0 com.zui.calendar.overlay.mains
+adb shell pm disable-user com.lenovo.leos.appstore
+adb shell pm disable-user com.lenovo.menu_assistant.hd
+adb shell pm disable-user com.android.providers.contacts
+adb shell pm disable-user com.zui.browser
 
 
-adb shell settings put system system_locales ja-JP || pause
+adb shell settings put system system_locales ja-JP
 timeout /T 2 /nobreak >nul
 adb reboot
 pause
